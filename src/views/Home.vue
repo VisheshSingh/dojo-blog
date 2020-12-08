855b1399c5e8b3e9e29051c477f7cd5cfc7e9415
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-07 21:04:41 -0600

    Template refs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="home">
     Composition API
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="paragraph">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
   name: 'Home',
   setup() {
     let name = 'v-boy';
     let age = 30;
+    let paragraph = ref(null);
 
     let handleClick = () => {
-      console.log('hi');
+      console.log(paragraph, paragraph.value);
+      paragraph.value.classList.add('test');
+      paragraph.value.textContent = 'wohuuuu!!!';
     };
 
-    return { name, age, handleClick };
+    return { name, age, handleClick, paragraph };
   },
 };
 </script>
