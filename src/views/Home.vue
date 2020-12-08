a70ae947971bef44c7672dda9e9f5cf76a2e63b4
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-07 21:01:12 -0600

    Composition API basics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="home">
-    home
+    Composition API
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  setup() {
+    let name = 'v-boy';
+    let age = 30;
+
+    let handleClick = () => {
+      console.log('hi');
+    };
+
+    return { name, age, handleClick };
+  },
 };
 </script>
